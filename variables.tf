a92808fa607bba95156b25b37c04ac210d3a03c3
issa <a07bagui@gmail.com>
2024-08-22 13:59:56 +0200

    Remove .pem file from repository

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "instance_type" {
   description = "The type of instance to launch"
   default     = "t2.micro"
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  description = "The type of instance to launch"
+
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "The type of instance to launch"
+  
+}
